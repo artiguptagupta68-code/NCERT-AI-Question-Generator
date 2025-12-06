65610ee1895004add22100cbbdc7ca7abe230be4
Arti Gupta <artigupta.gupta68@gmail.com>
2025-12-06 14:35:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -345,11 +345,10 @@ if st.button("Generate Questions") and topic.strip():
 
         # load generator
         generator = load_generator_pipeline()
-
-    def retrieve_chunks(query, index, metadata, top_k=5):
-    """
-    Retrieve the top_k most relevant chunks using FAISS search.
-    """
+        def retrieve_chunks(query, index, metadata, top_k=5):
+        """
+        Retrieve the top_k most relevant chunks using FAISS search.
+        """
 
     # Load embedding model
     model = SentenceTransformer(EMBEDDING_MODEL_NAME)
