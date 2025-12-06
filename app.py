eec64161a80e50c3f4a7cb260932dbc9a77ef7a4
Arti Gupta <artigupta.gupta68@gmail.com>
2025-12-06 14:49:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -345,6 +345,7 @@ if st.button("Generate Questions") and topic.strip():
 
         # load generator
     generator = load_generator_pipeline()
+   
     def retrieve_chunks(query, index, metadata, top_k=5):
         """
         Retrieve the top_k most relevant chunks based on FAISS similarity search.
@@ -357,8 +358,8 @@ if st.button("Generate Questions") and topic.strip():
 
     # Search FAISS index
     distances, indices = index.search(query_vec, top_k)
-
     # Collect relevant metadata
+    
     retrieved = []
     for idx in indices[0]:
         if 0 <= idx < len(metadata):
