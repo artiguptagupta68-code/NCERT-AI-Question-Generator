cda6ecf7c9e4a1bc602ebd65f4f313a0023e9ad5
Arti Gupta <artigupta.gupta68@gmail.com>
2025-12-11 14:43:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ except Exception:
     _HAS_PYMUPDF = False
 
 from sentence_transformers import SentenceTransformer
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitter import RecursiveCharacterTextSplitter
 import faiss
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
 
